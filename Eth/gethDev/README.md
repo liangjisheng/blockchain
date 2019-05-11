75cec5f12a931b7b9f99e2944d8be27683828446
liangjisheng <1294851990@qq.com>
2019-05-11 18:05:30 +0800

    geth develop

---
+++ Eth/gethDev/README.md
@@ -0,0 +1,7 @@
+## 用Go来做以太坊开发
+
+### 在线电子书
+
+[https://goethereumbook.org/zh/](https://goethereumbook.org/zh/)
+
+本书中，我们将使用Go的官方以太坊实现go-ethereum来和以太坊区块链进行交互。Go-ethereum，也被简称为Geth，是最流行的以太坊客户端。因为它是用Go开发的，当使用Golang开发应用程序时，Geth提供了读写区块链的一切功能
---
+++ Eth/gethDev/address.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func genEthAddr() {
+	key, _ := crypto.GenerateKey()
+	strPrikey := "b38acf64ad3b4d0a53b2a513b73170330af3d09cacc3d01a39d0a852abbd6313"
+	key.D.SetString(strPrikey, 16)
+
+	privateKey := hex.EncodeToString(key.D.Bytes())
+	strx := "9f3a050c7ec81b4450596f68d8251fab3accf3e8f2b6b07ea2a5b4a6bee02607"
+	stry := "06148ad1f756d64bd66ec2ce576302cc9b91e88a088271300bd7b9994f9670aa"
+	key.PublicKey.X.SetString(strx, 16)
+	key.PublicKey.Y.SetString(stry, 16)
+	address := crypto.PubkeyToAddress(key.PublicKey).Hex()
+
+	fmt.Printf("privateKey: 0x%s\n", privateKey)
+	fmt.Printf("addr: %s\n", address)
+}
